b8b51326fffafad7ae8d7c54c90bba42e0166eb1
ZafeerA123 <zafeer10ahmed@gmail.com>
2024-01-09 21:45:05 +0000

    New change1234432

--- assets/js/platformer/Controller.js
+++ assets/js/platformer/Controller.js
@@ -1,42 +1,35 @@
 import LocalStorage from "./LocalStorage.js";
 import GameEnv from "./GameEnv.js";
 import GameControl from "./GameControl.js";
-
 export class Controller extends LocalStorage{
     constructor(){
         var keys = {currentLevel:"currentLevel",gameSpeed:"gameSpeed"}; //default keys for localStorage
         super(keys); //creates this.keys
-        
     }
     //separated from constructor so that class can be created before levels are added
-    initialize(){ 
+    initialize(){
         this.loadAll(); // load data
-        
         if(this[this.keys.currentLevel]){ //update to active level
             GameControl.transitionToLevel(GameEnv.levels[Number(this[this.keys.currentLevel])]);
         }
         else{ //if not currentLevel then set this.currentLevel to 0 (default)
             this[this.keys.currentLevel] = 0;
         }
-
         if(this[this.keys.gameSpeed]){ //update to custom gameSpeed
            GameEnv.gameSpeed = Number(this[this.keys.gameSpeed]);
         }
         else{ //if not gameSpeedthen set this.gameSpeed to GameEnv.gameSpeed (default)
             this[this.keys.gameSpeed] = GameEnv.gameSpeed;
         }
-        
         window.addEventListener("resize",()=>{ //updates this.currentLevel when the level changes
             this[this.keys.currentLevel] = GameEnv.levels.indexOf(GameEnv.currentLevel);
             this.save(this.keys.currentLevel); //save to local storage
         });
-
         window.addEventListener("speed",(e)=>{ //updates this.gameSpeed when a speed event is fired
             this[this.keys.gameSpeed] = e.detail.speed();
             GameEnv.gameSpeed = this[this.keys.gameSpeed]; //reload or change levels to see effect
             this.save(this.keys.gameSpeed); //save to local storage
         })
- 
     }
     get levelTable(){
         var t = document.createElement("table");
@@ -49,7 +42,6 @@ export class Controller extends LocalStorage{
         th2.innerText = "Level Tag";
         header.append(th2);
         t.append(header);
-
         //create other rows
         for(let i = 0;i < GameEnv.levels.length;i++){
             var row = document.createElement("tr");
@@ -64,16 +56,13 @@ export class Controller extends LocalStorage{
             row.append(td2);
             t.append(row);
         }
-
         return t; //returns <table> element
     }
     get speedDiv(){
         var div = document.createElement("div"); //container
-
         var a = document.createElement("a"); //create text
         a.innerText = "Game Speed";
         div.append(a);
-
         var input1 = document.createElement("input"); //create inputfeild
         input1.type = "number"; //type number (1234...)
         const event = new CustomEvent("speed", { detail: {speed:()=>input1.value} });
@@ -81,9 +70,12 @@ export class Controller extends LocalStorage{
             window.dispatchEvent(event); //dispatch event to update game speed
         })
         div.append(input1);
-        
         return div; //returns <div> element
-    } 
+    }
 }
-
 export default Controller;
+
+
+
+
+
